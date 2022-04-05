91c9dc145083b13933a0786328a32773d12d3e51
czytobiegaczka <czytobiegaczka@gmail.com>
2022-04-05 14:55:55 +0200

    synchronizacja z pracy

---
+++ homework/Ex_1_8_logi.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+id_to_logged_in_time = {}
+id_to_login_times = defaultdict(list)
+id_to_logout_times = defaultdict(list)
+
+with open('logs.txt') as log_file:
+    for line in log_file:
+        username, operation, timestamp = line.split(';')
+        prefix, user_id = username.split('-')
+        user_id = int(user_id)
+
+        if operation == 'LOGIN':
+            id_to_login_times[user_id].append(int(timestamp))
+        elif operation == 'LOGOUT':
+            id_to_logout_times[user_id].append(int(timestamp))
+
+for id in id_to_login_times:
+    logged_in_time_for_user = 0
+    if len(id_to_login_times) != len(id_to_logout_times):
+        print(f"Nieprawidłowe dane w pliku. Różna ilość LOGIN i LOGOUT: {len(id_to_login_times)} != {len(id_to_logout_times)}")
+        break
+
+    for login, logout in zip(id_to_login_times[id], id_to_logout_times[id]):
+        if login > logout:
+            print(f'Nieprawidłowe dane wejściowe w pliku dla: user-{id}. Logout: {logout}, Login: {login}')
+            break
+
+        logged_in_time_for_user += logout - login
+
+    id_to_logged_in_time[id] = logged_in_time_for_user
+
+for id in sorted(id_to_logged_in_time):
+    print(f'user-{id:2}: {id_to_logged_in_time[id]:5} s')
--- odTomka/zad1_2.py
+++ odTomka/zad1_2.py
@@ -1,10 +1,8 @@
 def czy_tr(k):
-    if k[0] + k[1] > k[2] and k[1] + k[2] > k[0] and k[0] + k[2] > k[1] and k[0] > 0 and k[1] > 0 and k[2] > 0:
+    if k[0]+k[1]>k[2] and k[1]+k[2]>k[0] and k[0]+k[2]>k[1] and k[0]>0 and k[1]>0 and k[2]>0:
         return True
     else:
         return False
-
-
 def daj_ramie(i):
     while True:
         try:
@@ -12,8 +10,6 @@ def daj_ramie(i):
             return a
         except Exception:
             print('liczbę poproszę!!')
-
-
 
 while True:
     print('Podaj wymiary trójkąta')
@@ -24,6 +20,6 @@ while True:
         break
     else:
         print('z tego, to trójkąt nie wyjdzie!!!')
-ob = (tr[0] + tr[1] + tr[2]) / 2
-pole = (ob * (ob - tr[0]) * (ob - tr[1]) * (ob - tr[2])) ** 0.5
+ob=(tr[0]+tr[1]+tr[2])/2
+pole=(ob*(ob-tr[0])*(ob-tr[1])*(ob-tr[2]))**0.5
 print(f'pole trójkąta o wymiarach {tr[0]}, {tr[1]}, {tr[2]} wynosi: {pole:.2f}')
